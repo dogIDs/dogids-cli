dfd3ef99d63129ec2ca7e0353cf107e962ac496b
Brian Pattison <brian@brianpattison.com>
2015-02-14 17:14:15 -0600

    Add source ~/.codelation.bash to ~/.bash_profile

--- lib/codelation/development/dot_files.rb
+++ lib/codelation/development/dot_files.rb
@@ -6,12 +6,16 @@ module Codelation
 
     # Install dot files and load them into ~/.bash_profile
     def install_dot_files
+      # Copy dot files to user's home directory
       copy_file "dot_files/.codelation.bash",     "~/.codelation.bash"
       copy_file "dot_files/.git-completion.bash", "~/.git-completion.bash"
       copy_file "dot_files/.git-prompt.sh",       "~/.git-prompt.sh"
       copy_file "dot_files/.jshintrc",            "~/.jshintrc"
       copy_file "dot_files/.rubocop.yml",         "~/.rubocop.yml"
       copy_file "dot_files/.scss-lint.yml",       "~/.scss-lint.yml"
+
+      # Add `source ~/.codelation.bash` to ~/.bash_profile if it doesn't exist
+      append_to_file "~/.bash_profile", "source ~/.codelation.bash"
     end
   end
 end
