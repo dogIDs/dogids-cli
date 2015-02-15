628523506081b8356bfa1633f329844f9f9bd9da
Brian Pattison <brian@brianpattison.com>
2015-02-15 17:40:43 -0600

    Update version to 0.0.2

--- lib/codelation/version.rb
+++ lib/codelation/version.rb
@@ -3,7 +3,7 @@ require "open-uri"
 require "thor"
 
 module Codelation
-  VERSION = "0.0.1"
+  VERSION = "0.0.2"
 
   class Cli < Thor
     desc "update", "Update codelation-cli to latest version"
@@ -19,7 +19,7 @@ module Codelation
 
       # Grab the latest version of the RubyGem
       rubygems_json = open("https://rubygems.org/api/v1/gems/codelation-cli.json").read
-      rubygems_version = "v#{JSON.parse(rubygems_json)["version"].strip}"
+      rubygems_version = "v#{JSON.parse(rubygems_json)['version'].strip}"
 
       upgrade_message = ""
       if gem_version != rubygems_version
